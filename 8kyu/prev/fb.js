fd294d1a4b0148c233404af9a862f55305b188b4
Christian Kilpatrick <cckilpat@gmail.com>
2022-08-14 16:39:33 -0400

    cw 8/14

--- 8kyu/prev/fb.js
+++ 8kyu/prev/fb.js
@@ -4,36 +4,16 @@
 // E - fizzBuzz(3) --> 1, 2, fizz
 // P - check if % = 0 for 3,5,(3&5)
 
-const fizzBuzz = num => {
-    const numArr = Array.from(Array(num).keys())
-    console.log(numArr);
-    numArr.forEach(el => {
-        if (el%3 === 0 && el%5 === 0){
-            console.log('fizzbuzz');
-        }else if (el%3 === 0){
-            console.log('fizz');
-        }else if (el%5 === 0){
-            console.log('buzz');
-        }else{
-            console.log(el);
-        }
-    });
-}
-
-// fizzBuzz(28)
-
-function fB(num){
-    for (i=1;i<=num;i++){
+function fizzBuzz () {
+    for (let i = 1; i <= 100; i++){
         if (i%3 === 0 && i%5 === 0){
-            console.log('fizzbuzz');
+           console.log('fizzbuzz');
         }else if (i%3 === 0){
             console.log('fizz');
         }else if (i%5 === 0){
-            console.log('buzz');
-        }else{
+           console.log('buzz');
+        }else {
             console.log(i);
         }
     }
 }
-
-fB(45)
