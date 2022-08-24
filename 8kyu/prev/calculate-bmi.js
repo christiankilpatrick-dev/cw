38b750955ff9b41c05a3d943666d397b1606a36d
Christian Kilpatrick <cckilpat@gmail.com>
2022-08-24 11:53:21 -0400

    cw 8/24

--- 8kyu/prev/calculate-bmi.js
+++ 8kyu/prev/calculate-bmi.js
@@ -1,12 +1,12 @@
 function bmi(weight, height) {
-    let index = weight/height**2
-    if (index <= 18.5){
-      return "Youre Underweight"
-    }else if (index <= 25.0){
-      return 'Youre Normal'
-    }else if (index <= 30.0){
-      return "You're Overweight"
-    }else{
-        return "You're Obese";
-    }
-  }
+	let index = weight / height ** 2;
+	if (index <= 18.5) {
+		return 'Youre Underweight';
+	} else if (index <= 25.0) {
+		return 'Youre Normal';
+	} else if (index <= 30.0) {
+		return "You're Overweight";
+	} else {
+		return "You're Obese";
+	}
+}
