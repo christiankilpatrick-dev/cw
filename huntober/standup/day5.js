c523f7b358b86da0bd4ec966f3aadd645152177d
Christian Kilpatrick <cckilpat@gmail.com>
2022-10-20 14:50:31 -0400

    standup day5

---
+++ huntober/standup/day5.js
@@ -0,0 +1,19 @@
+// You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+//array of integers, some repeat some dont
+//a sum of all integers that occur only once
+//[4, 5, 7, 5, 4, 8] --> 7 + 8 ---> 15
+//create empty arr, loop through nums, if index of value === lastindexof value --> push to valid arr
+
+const repeats = (arr) => {
+	return arr
+		.filter((cv) => arr.indexOf(cv) === arr.lastIndexOf(cv))
+		.reduce((a, cv) => (a += cv));
+};
+
+console.log(repeats([4, 5, 7, 5, 4, 8]), 15);
+console.log(repeats([9, 10, 19, 13, 19, 13]), 19);
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]), 12);
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]), 22);
+console.log(repeats([5, 10, 19, 13, 10, 13]), 24);
