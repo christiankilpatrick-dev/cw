315105882a879b5a6c49f7df31e915e70613e28f
Christian Kilpatrick <cckilpat@gmail.com>
2022-11-15 18:17:06 -0500

    standup day15

--- huntober/standup/day15.js
+++ huntober/standup/day15.js
@@ -21,11 +21,16 @@ const sortArray = (arr) => {
 	return output;
 };
 
-console.log(sortArray([7, 1]), [1, 7]);
-console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4]);
+const sortedOdds = (arr) => {
+	let sorted = arr.filter((x) => x % 2 !== 0).sort((a, b) => a - b);
+	return arr.map((x) => (x % 2 === 0 ? x : sorted.shift()));
+};
+
+console.log(sortedOdds([7, 1]), [1, 7]);
+console.log(sortedOdds([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4]);
 console.log(
-	sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
+	sortedOdds([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]),
 	[1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 );
-console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
-console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+console.log(sortedOdds([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+console.log(sortedOdds([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
