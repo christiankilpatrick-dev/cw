98f0781821e16bd478d0ecb00ce6292aa80e7f59
Christian Kilpatrick <cckilpat@gmail.com>
2022-11-23 12:33:24 -0500

    cw 11/23

---
+++ languageSkills.js
@@ -0,0 +1,20 @@
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+const myLanguages = (results) => {
+	return Object.keys(results)
+		.filter((x) => results[x] > 59)
+		.sort((a, b) => results[b] - results[a]);
+};
+
+console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }), [
+	'Ruby',
+	'Python',
+]);
+console.log(myLanguages({ Hindi: 60, Dutch: 93, Greek: 71 }), [
+	'Dutch',
+	'Greek',
+	'Hindi',
+]);
+console.log(myLanguages({ 'C++': 50, ASM: 10, Haskell: 20 }), []);
