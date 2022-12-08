e3d24698faea14dc347fa9581ac6c38bbe4690b7
Christian Kilpatrick <cckilpat@gmail.com>
2022-12-08 18:24:23 -0500

    standup day24

--- huntober/standup/day24.js
+++ huntober/standup/day24.js
@@ -20,5 +20,24 @@ const max = (str) => {
 	}
 };
 
-console.log(max('Hello World!'), 'l');
-console.log(max('this is a test'), 't');
+const maxCharacter = (str) => {
+	let charMap = {},
+		count = 0,
+		maxChar = null;
+
+	for (let char of str) {
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+
+	for (let char in charMap) {
+		if (charMap[char] > count) {
+			count = charMap[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+};
+
+console.log(maxCharacter('Hello World!'), 'l');
+console.log(maxCharacter('this is a test'), 't');
