eecfc4ed623a0b334d9728a258417c83657388e3
Christian Kilpatrick <cckilpat@gmail.com>
2022-12-12 17:41:14 -0500

    standup day25

---
+++ huntober/standup/day25.js
@@ -0,0 +1,19 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+const uniqueSet = (nums) => {
+	let cache = [];
+	for (let i = 0; i < nums.length; i++) {
+		if (cache.includes(nums[i])) {
+			return true;
+		} else {
+			cache.push(nums[i]);
+		}
+	}
+	return false;
+};
+
+console.log(uniqueSet([1, 2, 3, 1]), true);
+
+console.log(uniqueSet([1, 2, 3, 4]), false);
+
+console.log(uniqueSet([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
