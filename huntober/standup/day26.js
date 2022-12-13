cd5a4730b193fc08e3f0a94cb185db4174e0b05d
Christian Kilpatrick <cckilpat@gmail.com>
2022-12-13 18:06:28 -0500

    standup day26

---
+++ huntober/standup/day26.js
@@ -0,0 +1,20 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+const majority = (nums) => {
+	let csh = {};
+	for (let i = 0; i < nums.length; i++) {
+		if (csh[nums[i]]) {
+			csh[nums[i]]++;
+		} else {
+			csh[nums[i]] = 1;
+		}
+	}
+
+	let max = Math.max(...Object.values(csh));
+	return Number(Object.keys(csh).find((key) => csh[key] === max));
+};
+
+console.log(majority([3, 2, 3]), 3);
+console.log(majority([2, 2, 1, 1, 1, 2, 2]), 2);
