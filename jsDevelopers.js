048bc13d9babe81e8da2ba8333906631bb0aeb49
Christian Kilpatrick <cckilpat@gmail.com>
2023-01-29 13:09:42 -0500

    cw 1/29

---
+++ jsDevelopers.js
@@ -0,0 +1,22 @@
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+const list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ]
+
+const countDevelopers = (arr) => {
+    let count = 0
+    arr.forEach((x, i) => {
+        if (x.language === 'JavaScript' && x.continent == 'Europe') { 
+            count++ 
+        } 
+    })
+    return count
+ } 
