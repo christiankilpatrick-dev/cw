3f32d2746a80cc856c615b1b7b3e6f2659d56540
Christian Kilpatrick <cckilpat@gmail.com>
2023-02-23 15:36:09 -0500

    cw 2/23

---
+++ computerProblem.js
@@ -0,0 +1,16 @@
+// Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+
+// Input:
+// Array of file sizes (0 <= s <= 100)
+// Capacity of the HD (0 <= c <= 500)
+// Output:
+// Number of files that can be fully saved in the HD.
+
+function save(sizes, hd) {
+  let i = -1;
+  while (hd >= 0) {
+    hd -= sizes.shift();
+    i++;
+  }
+  return i;
+}
