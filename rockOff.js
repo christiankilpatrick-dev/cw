839054ff58defe2c2b45f07b129349e02474c01b
Christian Kilpatrick <cckilpat@gmail.com>
2023-04-20 17:00:52 -0400

    cw 4/20

--- rockOff.js
+++ rockOff.js
@@ -12,4 +12,13 @@
 
 // The solution to the example above should therefore appear like '1, 2: Bob made "Jeff" proud!'.
 
-function solve(a, b) {}
+function solve(a, b) {
+  let [x, y] = [0, 0];
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) x++;
+    else if (a[i] < b[i]) y++;
+  }
+  if (x > y) return `${x}, ${y}: Alice made "Kurt" proud!`;
+  else if (x < y) return `${x}, ${y}: Bob made "Jeff" proud!`;
+  else return `${x}, ${y}: that looks like a "draw"! Rock on!`;
+}
