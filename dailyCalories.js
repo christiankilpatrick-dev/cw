9ebdd86d6391893dbdb8d64f919fa7af5df5fb58
Christian Kilpatrick <cckilpat@gmail.com>
2023-04-29 11:42:34 -0400

    cw 4/29

---
+++ dailyCalories.js
@@ -0,0 +1,17 @@
+// A gym plans to offer a new service campaign about nutrition advice in relation to sporting activities. Initially each member´s daily calorie requirement has to be calculated.
+
+// There are various ways of calculating the amounts of calories needed to maintain energy balance for various gender and age groups at different levels of physical activity. One of the most accurate formulas used to calculate a person´s daily calorie demand is the Mifflin-St Jeor formula.
+
+// Before applying the formula it is important to know a person´s gender, weight (kg), height (cm), age (y) and activity factor. By knowing all of these factors the calculation can be done in two simple steps:
+
+// Step 1: Calculation of “Resting Metabolic Rate” (RMR):
+
+// RMR Men = 10 x weight (kg) + 6.25 x height (cm) - 5 x age (y) + 5
+// or
+// RMR Women = 10 x weight (kg) + 6.25 x height (cm) - 5 x age (y) – 161
+// Step 2: RMR multiplied by activity factor:
+
+// a) little activity, activity factor = 1.2
+// b) moderately active,  activity factor  = 1.55
+// c) very active,  activity factor  = 1.7
+// d) extremely active,  activity factor = 1.9
