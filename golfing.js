f03e623cd9512d390a44c4c5c17c006fd70a8a23
Christian Kilpatrick <cckilpat@gmail.com>
2023-05-04 10:39:52 -0400

    cw 5/4

--- golfing.js
+++ golfing.js
@@ -8,3 +8,11 @@
 
 // Task Overview
 // Complete the function which accepts two strings and calculates the golf score of a game. Both strings will be of length 18, and each character in the string will be a number between 1 and 9 inclusive.
+
+function golfScoreCalculator(parList, scoreList) {
+  result = 0;
+  for (let i = 0; i < parList.length; ++i) {
+    result += scoreList[i] - parList[i];
+  }
+  return result;
+}
