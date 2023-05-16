8fa99f9bdb790d3e7b45ac077a69a286042abb84
Christian Kilpatrick <cckilpat@gmail.com>
2023-05-16 18:30:55 -0400

    cw 5/16

--- groupingString.js
+++ groupingString.js
@@ -9,3 +9,14 @@
 // Explanation
 // In first example same elements are grouped together: (11)(22)(33), hence it should return true
 // In the second example there are two different groups of 1's: (11)2233(11), hence it should return false
+
+const isConsecutive = (str) => {
+  const seen = new Set();
+  let latest;
+
+  for (const char of str)
+    if (!seen.has(char)) seen.add((latest = char));
+    else if (latest !== char) return false;
+
+  return true;
+};
