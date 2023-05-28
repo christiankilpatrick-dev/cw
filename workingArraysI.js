199b8f8a42772886731bb4513f54e490eb4f9805
Christian Kilpatrick <cckilpat@gmail.com>
2023-05-28 18:47:26 -0400

    cw 5/28

---
+++ workingArraysI.js
@@ -0,0 +1,11 @@
+// In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+// Good luck!
+
+// Hint: watch out for side effects.
+
+function withoutLast(arr) {
+  var arr2 = arr.slice();
+  arr2.pop();
+  return arr2;
+}
