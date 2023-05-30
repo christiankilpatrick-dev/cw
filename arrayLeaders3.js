ac830537cba4bde9c3e993c61c7815ae1af27f6f
Christian Kilpatrick <cckilpat@gmail.com>
2023-05-30 18:44:35 -0400

    cw 5/30

---
+++ arrayLeaders3.js
@@ -0,0 +1,13 @@
+// Given an array/list [] of integers , Find all the LEADERS in the array.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+// Input >> Output Examples
+// arrayLeaders ({1, 2, 3, 4, 0}) ==> return {4}
